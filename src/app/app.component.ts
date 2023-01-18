21a6b57673e49732348e12b8a6053c85f2e10487
sudarmanto <sudarmanto.atmo@gmail.com>
2023-01-18 22:43:13 +0700

    19. Generating a Random Password

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,7 +17,6 @@ export class AppComponent {
     
     if (!isNaN(parsedValue)) {
       this.length = parsedValue;
-      console.log(this.length);
     }
   }
 
@@ -34,12 +33,28 @@ export class AppComponent {
   }
 
   onButtonClick() {
-    console.log(`
-      About go generate a password with the following:
-      Includes letters: ${this.includeLetters}
-      Includes numbers: ${this.includeNumbers}
-      Includes symbols: ${this.includeSymbols}
-    `);
-    this.password = 'MY PASSWORD!!!';
+    const numbers = '1234567890';
+    const letters = 'abcdefghijklmnopqrstuvwxyz';
+    const symbols = '!@#$%^&*()';
+
+    let validChars = '';
+
+    if (this.includeLetters) {
+      validChars += letters;
+    }
+    if (this.includeNumbers) {
+      validChars += numbers;
+    }
+    if (this.includeSymbols) {
+      validChars += symbols;
+    }
+
+    let generatedPassword = '';
+    for (let i = 0; i< this.length; i++) {
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index];
+    }
+    this.password = generatedPassword;
+
   }
 }
